cd7d34e569e2a0e13773bacf2572a0c72bc33c45
AnymoreProject <anymore.dev13@gmail.com>
2024-07-08 18:39:25 +0000

    Revert "msm_performance: don't limit min and max cpu freq"
    This reverts commit 3029413690d3dba518522907a9b01e1aa1355f6a.

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -27,8 +27,6 @@
 #include <linux/kthread.h>
 #include <linux/sched/core_ctl.h>
 
-static int touchboost = 1;
-
 /*
  * Sched will provide the data for every 20ms window,
  * will collect the data for 15 windows(300ms) and then update
@@ -59,11 +57,13 @@ static int touchboost = 1;
 /*******************************sysfs start************************************/
 static int set_touchboost(const char *buf, const struct kernel_param *kp)
 {
-	int val;
-	if (sscanf(buf, "%d\n", &val) != 1)
+	int cnt, val;
+
+	cnt = sscanf(buf, "%d\n", &val);
+	if (cnt != 1)
 		return -EINVAL;
 	touchboost = val;
-	return 0;
+	return cnt;
 }
 
 static int get_touchboost(char *buf, const struct kernel_param *kp)
@@ -84,9 +84,6 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
 
-	if (touchboost == 0)
-		return 0;
-
 	int ret = 0;
 
 	if (!touchboost) {
@@ -102,7 +99,6 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 		return -EINVAL;
 
 	cp = buf;
-
 	cpumask_clear(limit_mask);
 	for (i = 0; i < ntokens; i += 2) {
 		if (sscanf(cp, "%u:%u", &cpu, &val) != 2)
@@ -171,9 +167,6 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
 
-	if (touchboost == 0)
-		return 0;
-
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
 
@@ -182,7 +175,6 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 		return -EINVAL;
 
 	cp = buf;
-
 	cpumask_clear(limit_mask);
 	for (i = 0; i < ntokens; i += 2) {
 		if (sscanf(cp, "%u:%u", &cpu, &val) != 2)
